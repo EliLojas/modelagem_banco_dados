89cffbeef697bb82af4940461235aaadc2e2c10a
Bruno Nogueira Silva <bruno.silva@credisis.com.br>
2023-10-31 11:25:37 -0300

    Alteração dos nome do projeto na documentação

--- doc-modelagem_banco_dados/docusaurus.config.js
+++ doc-modelagem_banco_dados/docusaurus.config.js
@@ -1,17 +1,17 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
-const nomeOrganizacao = "ONiNove";
+const nomeOrganizacao = "EliLojas";
 const nomeProjeto = "modelagem_banco_dados";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'BD i9',
-  tagline: 'Modelagem do Banco de Dados',
-  favicon: 'img/i9.ico',
+  title: "BD Eli Lojas",
+  tagline: "Modelagem do Banco de Dados",
+  favicon: "img/i9.ico",
 
   // Set the production url of your site here
   url: `https://${nomeOrganizacao}.github.io`,
@@ -21,41 +21,39 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ONiNove', // Usually your GitHub org/user name.
-  projectName: 'modelagem_banco_dados', // Usually your repo name.
+  organizationName: "EliLojas", // Usually your GitHub org/user name.
+  projectName: "modelagem_banco_dados", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'pt-BR',
-    locales: ['pt-BR'],
+    defaultLocale: "pt-BR",
+    locales: ["pt-BR"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            `https://github.com/${nomeOrganizacao}/${nomeProjeto}/tree/main/`,
+          editUrl: `https://github.com/${nomeOrganizacao}/${nomeProjeto}/blob/main/doc-modelagem_banco_dados/`,
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            `https://github.com/${nomeOrganizacao}/${nomeProjeto}/tree/main/`,
+          editUrl: `https://github.com/${nomeOrganizacao}/${nomeProjeto}/blob/main/`,
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -65,78 +63,88 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'BD i9',
+        title: "BD Eli Lojas",
         logo: {
-          alt: 'Modelagem BD',
-          src: 'img/logo.svg',
+          alt: "Modelagem BD",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Tutorial",
           },
           {
-            to: '/blog',
-            label: 'Blog',
-            position: 'left'
+            to: "/blog",
+            label: "Blog",
+            position: "left",
           },
           {
             href: `https://github.com/${nomeOrganizacao}/${nomeProjeto}`,
-            label: 'GitHub',
-            position: 'right',
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: "Tutorial",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: 'Comunidade',
+            title: "Comunidade",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
               },
             ],
           },
           {
-            title: 'Mais',
+            title: "Mais",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: "Blog",
+                to: "/blog",
               },
               {
-                label: 'GitHub',
+                label: "GitHub",
                 href: `https://github.com/${nomeOrganizacao}/${nomeProjeto}`,
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Modelagem de Banco de Dados, Inc. Construido por i9.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Modelagem de Banco de Dados, Inc. Construido por Eli Lojas.`,
       },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+
+      mermaid: {
+        theme: { light: "neutral", dark: "forest" },
+      },
     }),
+
+  markdown: {
+    mermaid: true,
+  },
+
+  themes: ["@docusaurus/theme-mermaid"],
 };
 
 module.exports = config;
